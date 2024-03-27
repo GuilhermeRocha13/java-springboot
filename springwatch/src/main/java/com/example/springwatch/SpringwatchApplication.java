d4b9476d9c6a3bc555a0e5829c3fe0f4ce83d99b
Guilherme Henrique <guilherme_h2006@hotmail.com>
2024-03-27 10:38:53 -0300

    env config

--- springwatch/src/main/java/com/example/springwatch/SpringwatchApplication.java
+++ springwatch/src/main/java/com/example/springwatch/SpringwatchApplication.java
@@ -1,5 +1,6 @@
 package com.example.springwatch;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import com.example.springwatch.services.ApiConsum;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,13 +13,17 @@ public class SpringwatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringwatchApplication.class, args);
+
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 
+		Dotenv dotenv = Dotenv.load();
+		String apiKey = dotenv.get("API_KEY");
+
 		var apiconsum = new ApiConsum();
-		var json = apiconsum.dataGet("https://www.omdbapi.com/?t=transformers&apikey=9e27a9fb");
+		var json = apiconsum.dataGet("https://www.omdbapi.com/?t=transformers&apikey=" + apiKey);
 
 		System.out.println(json);
 		dataConvert converter = new dataConvert();
